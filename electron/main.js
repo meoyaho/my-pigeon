bd0f81f1a1f6cfcc5ba2ab633a1d978d77c2e3e1
jisuryou <alji7@naver.com>
2026-08-04 22:28:18 +0900

    chore: scaffold electron project with IPC channel constants

---
+++ electron/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, '..', 'renderer', 'index.js'),
+    },
+  });
+  win.loadFile(path.join(__dirname, '..', 'renderer', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
